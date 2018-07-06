9d6649d87bb827f96b70a4ff0761964de94806a7
Brennan Colberg <brennan.colberg@gmail.com>
2018-07-06 09:36:08 -0700

    Basic Universal Header
    - added code index.html & logos/index.html
    - added entire logos folder
    - added header.js and header.css
    - made header.js generate a header
    KEY LEARNING: for universal JS & CSS linkage, use urls in "/folder/file.ext" form; the first slash indicates "from root of website" whereas without it it indicates "from current directory" (the latter changes from different linkage locations, hence is bad)
    - yes, I know that the links look horrible (it's a testing prototype)

---
+++ header.js
@@ -0,0 +1,50 @@
+// contains all code necessary for generating a universal header
+// put no code here NOT necessary for the header; make new file instead
+"use strict"; // makes better errors
+(function() { // avoid global declaration
+	
+	const navLinks = [
+		["Home", "/index.html"],
+		["Logos", "/logos/index.html"]
+	];
+	
+	// 	STRUCTURE OF HEADER:
+	//	body
+	//	 header
+	//	  logo
+	//	  navBar
+	//	   div
+	//	    a (link)
+	//	   div
+	//	    a (link)
+	//	   ...
+	
+	// encapsulated setup
+	window.addEventListener("load", function() {
+		let header = document.querySelector("header");
+		
+		// logo
+		let logo = document.createElement("img");
+		logo.id = "logo";
+		logo.alt = "Valhallabots - Team 3268";
+		logo.src = window.location.origin + "/logos/Logo_Text.png";
+		header.appendChild(logo);
+		
+		// navigation bar
+		let navBar = document.createElement("div");
+		navBar.id = "navBar";
+		header.appendChild(navBar);
+		// links in navigation bar
+		for (let i = 0; i < navLinks.length; i++) {
+			let navLinkDiv = document.createElement("div");
+			let navLink = document.createElement("a");
+			navLink.innerHTML = navLinks[i][0];
+			navLink.href = navLinks[i][1];
+			navBar.appendChild(navLink);
+			navLinkDiv.appendChild(navLink);
+			navBar.appendChild(navLinkDiv);
+		}
+		
+	});
+	
+})();
