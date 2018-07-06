f07618657e253219c7446b71cd46168d6c60c1a8
Brennan Colberg <brennan.colberg@gmail.com>
2018-07-06 11:21:02 -0700

    forms.html, about.html & More Formatting
    added HTML content for forms & about, did more with various formatting & JS things (mainly focused on article formatting)

---
+++ article.js
@@ -0,0 +1,14 @@
+"use strict";
+(function() {
+	
+	/* window.addEventListener("load", function() {
+		let article = document.querySelector("script");
+		let videos = document.querySelectorAll("article iframe.youtube");
+		for (let i = 0; i < videos.length; i++) {
+			videos[i].syle.width = article.width;
+			videos[i].style.height = article.width * (340/560);
+		}
+		alert("load");
+	}); */
+	
+})();
