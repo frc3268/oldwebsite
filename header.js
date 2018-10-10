71312e0d35ac648d49612949d2341c3b50f95593
Brennan Colberg <brennan.colberg@gmail.com>
2018-10-10 00:16:02 -0700

    added new page to menu & renamed to "coding"

--- header.js
+++ header.js
@@ -1,17 +1,18 @@
 // contains all code necessary for generating a universal header
 // put no code here NOT necessary for the header; make new file instead
 "use strict"; // makes better errors
-(function() { // avoid global declaration
-	
+(function () { // avoid global declaration
+
 	const navLinks = [
 		["Home", "index.html"],
+		["FAQ", "faq.html"],
+		["About", "about.html"],
 		["Press", "videos.html"],
+		["Coding", "coding.html"],
 		["Forms", "forms.html"],
-		["Logos", "logos.html"],
-		["FAQ","faq.html"],
-		["About", "about.html"]
+		["Logos", "logos.html"]
 	];
-	
+
 	// 	STRUCTURE OF HEADER:
 	//	body
 	//	 header
@@ -22,18 +23,18 @@
 	//	   div
 	//	    a (link)
 	//	   ...
-	
+
 	// encapsulated setup
-	(function() {
+	(function () {
 		let header = document.querySelector("header");
-		
+
 		// logo
 		let logo = document.createElement("img");
 		logo.id = "logo";
 		logo.alt = "Valhallabots - Team 3268";
 		logo.src = window.location.origin + "/logos/Logo_Text.png";
 		header.appendChild(logo);
-		
+
 		// navigation bar
 		let navBar = document.createElement("div");
 		navBar.id = "navBar";
@@ -48,7 +49,7 @@
 			navLinkDiv.appendChild(navLink);
 			navBar.appendChild(navLinkDiv);
 		}
-		
+
 	})();
-	
+
 })();
