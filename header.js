851f9f8fa7e209b98ee272fa48197d0cc6bf0d3a
fsoohyuncarter <fsoohyuncarter@gmail.com>
2022-04-19 10:50:53 -0700

    :()

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 /*Navbar info is stored here to make it easier to update it/add mobile compatibility, inspired from Brennan's work*/
 /*sorry I deleted all the old code, if you want to view it just look at the history*/
 
-/*To add in new pages, just add an image into the navbar folder and put it in, copying the format of the other imgs (no background, white img, 64*64px). */
+/*To add in new pages, just add an image into the navbar folder and put it in, COPYING THE FORMAT of the other selects and imgs (no background, white img, 64*64px). */
 const navMobile = `
 <style>
 #main {
@@ -35,7 +35,7 @@ div {
 <a class="navIcon" href='faq.html'><img src='navbar/faq.png'> <h3>Common Qs</h3> </a>
 <a class="navIcon" href='coding.html'><img src='navbar/code2.png'><h3>Coding</h3></a>
 <a class="navIcon" href='https://discord.com/invite/Gt6eUeGbAh'><img src='navbar/discord.png'> <h3>Discord</h3> </a>
-<a href='coding.html'><img src='navbar/history.png'></a> <h3>History</h3>
+<a class="navIcon" href='coding.html'><img src='navbar/history.png'> <h3>History</h3></a>
 
 `;
 
